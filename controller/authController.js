6e76f2dfdbc7571612ce13efa1f2ac91cde3bab8
Godcoder2005 <202311059@diu.iiitvadodara.ac.in>
2025-03-10 11:03:39 +0530

    authController

--- controller/authController.js
+++ controller/authController.js
@@ -1,27 +1,24 @@
 import userModel from "../models/Usermode.js"
 
-export const Authentication = async (req, res) => {
+export const Authentication = async (req, res, next) => {
     try {
         const { name, email, password } = req.body;
 
         //Validation process
         if (!name) {
-            return res.status(400).send({ message: "Please provide a name", success: false });
+           next('Name is required');
         }
         if (!email) {
-            return res.status(400).send({ message: "Please provide an email", success: false });
+            next('Email is required');
         }
         if (!password) {
-            return res.status(400).send({ message: "Please provide a password", success: false });
+            next('password is required and greater than 6 character');
         }
 
         // Check if the user already exists
         const existingUser = await userModel.findOne({ email });
         if (existingUser) {
-            return res.status(409).send({
-                success: false,
-                message: "Email already exists, please login."
-            });
+           next('Email exist do direct login')
         }
 
         // if not Create a new user
@@ -33,11 +30,6 @@ export const Authentication = async (req, res) => {
         });
 
     } catch (error) {
-        console.error("Error in Register Control:", error);
-        return res.status(500).send({
-            success: false,
-            message: "Internal server error",
-            error: error.message
-        });
+       next(error)
     }
 };
